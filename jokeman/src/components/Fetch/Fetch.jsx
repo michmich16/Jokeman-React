27f585437b27a36deca5f668fafdca692e63bd56
Michael P <102651473+michmich16@users.noreply.github.com>
2024-09-02 13:55:44 +0200

    done

--- jokeman/src/components/Fetch/Fetch.jsx
+++ jokeman/src/components/Fetch/Fetch.jsx
@@ -3,13 +3,16 @@ import s from './Fetch.module.scss';
 
 export function Fetch() {
     const [apiData, setApiData] = useState();
-    const url = `https://official-joke-api.appspot.com/jokes/random`
+     const url = `https://uselessfacts.jsph.pl/api/v2/facts/random`
+    // const url = `https://official-joke-api.appspot.com/jokes/random`
+
+    //jeg afkommenteret joke api fordi jeg blev ved med at får too many requests fejl😢😢😢
 
     async function getRandomData() {
         let res = await fetch(url);
         let data = await res.json();
         setApiData(data);
-         console.log(apiData)
+        console.log(apiData)
     }
 
     useEffect(() => {
@@ -20,8 +23,9 @@ export function Fetch() {
         <>
             <section className={s.dataStyle}>
                 {apiData ? <div>
-                    <p>🧐{apiData.setup}</p>
-                    <p>😂{apiData.punchline}</p>
+                    {/* <p>🧐{apiData.setup}</p>
+                    <p>😂{apiData.punchline}</p> */}
+                    <p>{apiData.text}</p>
                 </div> : <p>Loading... please wait</p>}
 
                 <div>
